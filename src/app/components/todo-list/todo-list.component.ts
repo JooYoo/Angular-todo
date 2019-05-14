4d887b2ccf71b0e5f675dee6aef3f30a405788ba
JooYoo <jooyoo@outlook.com>
2019-05-14 15:56:02 +0200

    add todo works

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  todos: object[]
+  todoTitle: string
+  idForTodo: number
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todoTitle =''
+    this.idForTodo = 4
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing': false,
+      },
+    ];
+
+  }
+
+  addTodo():void{
+    if (this.todoTitle.trim().length=== 0) {
+      return
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title:this.todoTitle,
+      completed:false,
+      editing:false
+    })
+
+    this.todoTitle = ''
+    this.idForTodo++;
+  }
+
+}
